ac9fe79f5ef5963b4a8288dd2ec7814cfb1909de
KidmanXD <112696687+KidmanXD@users.noreply.github.com>
2023-03-14 18:32:04 +0200

    Update Resistor.py

--- resistor/Resistor.py
+++ resistor/Resistor.py
@@ -1,9 +1,9 @@
 class Resistor():
     """
-    Класс Resistor представляет собой резистор, который определяется по цветовому коду на корпусе.
-    Цветовой код состоит от 2 до 5 полос, которые определяют сопротивление резистора и его допустимую погрешность.
+        Класс Resistor представляет собой резистор, который определяется по цветовому коду на корпусе.
+        Цветовой код состоит от 2 до 5 полос, которые определяют сопротивление резистора и его допустимую погрешность.
     """
-    
+
     def __init__(self, *colors):
         self.colors = colors
         self.color_map = {
@@ -23,9 +23,8 @@ class Resistor():
 
     def resistance(self):
         """
-        Вычисляет сопротивление резистора, используя заданный цветовой код.
+            Вычисляет сопротивление резистора, используя заданный цветовой код.
         """
-        
         if len(self.colors) == 4:
             multiplier = 10 ** self.color_map[self.colors[3]]
         elif len(self.colors) == 5:
@@ -33,32 +32,59 @@ class Resistor():
         else:
             multiplier = 1
 
-        return (self.color_map[self.colors[0]] * 10 + self.color_map[self.colors[1]]) * multiplier
+        return round((self.color_map[self.colors[0]] * 10 + self.color_map[self.colors[1]]) * multiplier)
 
     def tolerance(self):
         """
-        Определяет допустимую погрешность резистора, используя заданный цветовой код.
-        Если указаны 4 цветовые полоски, то это означает, что резистор без указания точности, и допустимая погрешность будет 20%.
-        Если указаны 5 цветовых полосок и последняя полоска имеет цвет "золотой", то допустимая погрешность составляет 5%.
-        Если указаны 5 цветовых полосок и последняя полоска имеет цвет "серебро", то допустимая погрешность составляет 10%.
+            Определяет допустимую погрешность резистора, используя заданный цветовой код.
         """
-        
-        if len(self.colors) == 4:
-            return 20
+
+        if len(self.colors) == 4 and self.colors[3] == "серебро":
+            return 10
+        elif len(self.colors) == 4 and self.colors[3] == "золотой":
+            return 5
+        elif len(self.colors) == 4 and self.colors[3] == "коричневый":
+            return 1
+        elif len(self.colors) == 4 and self.colors[3] == "красный":
+            return 2
+        elif len(self.colors) == 4 and self.colors[3] == "зеленый":
+            return 0.5
+        elif len(self.colors) == 4 and self.colors[3] == "синий":
+            return 0.25
+        elif len(self.colors) == 4 and self.colors[3] == "фиолетовый":
+            return 0.1
+        elif len(self.colors) == 4 and self.colors[3] == "серый":
+            return 0.05
+        elif len(self.colors) == 4:
+            return None
+
         elif len(self.colors) == 5 and self.colors[4] == "золотой":
             return 5
         elif len(self.colors) == 5 and self.colors[4] == "серебро":
             return 10
+        elif len(self.colors) == 5 and self.colors[4] == "коричневый":
+            return 1
+        elif len(self.colors) == 5 and self.colors[4] == "красный":
+            return 2
+        elif len(self.colors) == 5 and self.colors[4] == "зеленый":
+            return 0.5
+        elif len(self.colors) == 5 and self.colors[4] == "синий":
+            return 0.25
+        elif len(self.colors) == 5 and self.colors[4] == "фиолетовый":
+            return 0.1
+        elif len(self.colors) == 5 and self.colors[4] == "серый":
+            return 0.05
+        elif len(self.colors) == 4:
+            return None
         else:
             return None
 
 
 if __name__ == "__main__":
-    colors = input("Введите цветовой код резистора через пробел (например: красный желтый зеленый)"
-                   "\nМожно вести от 2 до 5 цветового кода"
-                   "\nЕсли на резисторе 5 полос то на 5 полосе можно водить еще такие цвета (золотой и серебро) для указания погрешности \n").lower().split()
+    colors = input("Введите цветовой код резистора через пробел (например: красный желтый зеленый). Можно вести от 2 до 5 цветового кода. \n"
+                   "Если на резисторе 4 полосы то на 4 полосе можно водить еще такие цвета (золотой и серебро) для указания точности, тоже самое для 5-и полос. \n").lower().split()
 
 
     resistor = Resistor(*colors)
-    print("Сопротивление резистора:", resistor.resistance(), "Ом")
+    print("Сопротивление резистора:", resistor.resistance(), "Ом;", (resistor.resistance()/1000), "кОм;",(resistor.resistance()/1000000), "мОм;", (resistor.resistance()/1000000000), "ГОм;")
     print("Допустимая погрешность:", resistor.tolerance(), "%")
